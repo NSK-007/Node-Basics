e260bc111f100ed655e1a9d508309fd4220e384f
shiva-kumar-nadigoti <nshiva900@gmail.com>
2023-02-16 09:06:57 +0530

    routing and redirecting

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 const http = require('http')
+const fs = require('fs');
 const server = http.createServer((req, response) => {
-    if(req.url==='/node')
+    response.setHeader('Content-Type', 'text/html');
+    if(req.url==='/')
     {
         response.write('<html>');
         response.write('<body>');
-        response.write('<h1>Welcome to my node js project</h1>');
+        response.write('<form action="/message" method="POST"><input type="text" name="message"><input type="submit" value="Submit"></form>');
         response.write('</body>');
         response.write('</html>');
-        response.end();
+        return response.end();
     }
 
-    if(req.url==='/home'){
-        response.write('<html>');
-        response.write('<body>');
-        response.write('<h1>Welcome home</h1>');
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
+    if(req.url==='/message' && req.method==='POST'){
+        fs.writeFileSync('message.txt', 'DUMMY 3');
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
+        return response.end();
     }
-    if(req.url==='/about'){
-        response.write('<html>');
-        response.write('<body>');
-        response.write('<h1>Welcome to About Us page</h1>');
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
-    }
-    // setTimeout(() => {
-    //     console.log('Exiting...')
-    //     process.exit();
-    // }, 3000)
 })
 
 server.listen(4000);
